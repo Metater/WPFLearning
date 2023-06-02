b1f274cb9f25fc604b9bc96ec53e7c570042466e
Connor <61102529+Metater@users.noreply.github.com>
2023-06-02 11:58:18 -0400

    Add project files.

---
+++ WPFLearning/ViewModel/ControlsPaneViewModel.cs
@@ -0,0 +1,25 @@
+﻿using WPFLearning.Model;
+using WPFLearning.MVVM;
+
+namespace WPFLearning.ViewModel
+{
+    internal class ControlsPaneViewModel : ViewModelBase
+    {
+        public ControlsPaneViewModel()
+        {
+            
+        }
+
+        private ControlsSettings settings;
+
+        public ControlsSettings Settings
+        {
+            get { return settings; }
+            set
+            {
+                settings = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
